cb3e68186baa9b42a45fdf4af71f087c71845a8e
Jaeyoon <wodbs4338@naver.com>
2021-06-12 03:53:12 +0900

    비즈니스 로직 수행 결과를 나타낼 클래스 생성

---
+++ src/main/java/webserver/ExecutionResult.java
@@ -0,0 +1,19 @@
+package webserver;
+
+public class ExecutionResult {
+    private ResponseType responseType;
+    private Object returnData;
+
+    public ExecutionResult(ResponseType responseType, Object returnData) {
+        this.responseType = responseType;
+        this.returnData = returnData;
+    }
+
+    public ResponseType getResponseType() {
+        return responseType;
+    }
+
+    public Object getReturnData() {
+        return returnData;
+    }
+}
