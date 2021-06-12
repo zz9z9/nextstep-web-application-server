aa184d0230f5ef169ed56b6034c5ffd640604623
Jaeyoon <wodbs4338@naver.com>
2021-06-13 03:16:05 +0900

    로그인 성공/실패시 쿠키값 세팅

--- src/main/java/logic/UserLogic.java
+++ src/main/java/logic/UserLogic.java
@@ -2,6 +2,9 @@ package logic;
 
 import db.DataBase;
 import model.User;
+import webserver.HttpResponse;
+
+import java.io.IOException;
 
 // TODO : 싱글톤 패턴 멀티스레드 등 고려하는 방식으로 리팩토링하기
 public class UserLogic {
@@ -18,13 +21,14 @@ public class UserLogic {
         return "/index.html";
     }
 
-    public String login(String id, String pw) {
+    public String login(String id, String pw, HttpResponse response) throws IOException {
         User findUser = DataBase.findUserById(id);
-
         if(findUser!=null && pw.equals(findUser.getPassword())) {
+            response.setCookie("logined=true");
             return "/index.html";
         }
 
+        response.setCookie("logined=false");
         return "/user/login_failed.html";
     }
 }
